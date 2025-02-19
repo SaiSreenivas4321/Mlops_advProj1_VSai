42c600b4634da2dbb1ca0ac5a8d9e9c88766712c
SaiSreenivas_nehask_Datascitist <sreenivassaibks@gmail.com>
2025-02-20 02:32:57 +0530

    All files and folder has been created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages 
+
+setup(
+    name="Proadv",
+    version="0.0.1",
+    author ="Sai Sreenivas",
+    author_email = "sreenivassaibks@gmail.com",
+    packages=find_packages()
+)
